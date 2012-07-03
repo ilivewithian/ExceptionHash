1cd23a951be5e22ffd592fdb98ffabb6ac1fd1d8
Rob White <rob.j.white@gmail.com>
2012-07-03 21:58:15 +0100

    Initial Commit

---
+++ ExceptionHasher.Tests/HashTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using ExceptionHash;
+using NUnit.Framework;
+
+namespace ExceptionHash.Tests
+{
+    [TestFixture]
+    public class HashTests
+    {
+        [Test]
+        public void When_Exception_Is_Null_Throw()
+        {
+            Exception target = null;
+
+            Assert.Throws<NullReferenceException>(() => target.Hash());
+        }
+
+        [Test]
+        public void Ensure_Message_IsHashed()
+        {
+            var expectedHash = string.Format("{0:X8}", "Fail Message!".GetHashCode());
+
+            var target = new Exception("Fail Message!");
+
+            Assert.That(expectedHash, Is.EqualTo(target.Hash()));
+        }
+
+        [Test]
+        public void Ensure_InnerException_IsHashed()
+        {
+            var expectedHash = string.Format("{0:X8}", "Fail Message!Inner Fail!".GetHashCode());
+
+            var target = new Exception("Fail Message!", new Exception("Inner Fail!"));
+
+            Assert.That(expectedHash, Is.EqualTo(target.Hash()));
+        }
+
+        [Test]
+        public void Ensure_Stack_Trace_Is_Included()
+        {
+            try
+            {
+                throw new Exception("Fail Message!");
+            }
+            catch (Exception target)
+            {
+                //Ok so this horrible code is the easiest way to create an exception with a stack trace
+
+                var expectedHash = string.Format("{0:X8}", "Fail Message!   at ExceptionHash.Tests.HashTests.Ensure_Stack_Trace_Is_Included() in C:\\Users\\Rob\\ExceptionHash\\ExceptionHasher.Tests\\HashTests.cs:line 43".GetHashCode());           
+
+                Assert.That(expectedHash, Is.EqualTo(target.Hash()));
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-ExceptionHash
+Exception Hash
 =============
+
+Exception Hash is a simple extension that allows you to create a hash for an exception.
+
+The hash is based on the exceptions message, stack trace and inner exceptions.
+
+This kind of hash is usefull if you want an easy way to identify a specific reocuring exception in your error tracking database.
+
+For example all NullReferenceExceptions thrown from the method `ParseInt` will have the same hash making them easy to track down.
